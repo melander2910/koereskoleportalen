85d898ab601d330616407f654cdf3859e062bb20
kris52h6 <kris52h6@stud.kea.dk>
2024-04-10 13:00:01 +0200

    added 'DrivingSchool' as its own object

--- Portal.API/Models/DrivingSchool.cs
+++ Portal.API/Models/DrivingSchool.cs
@@ -4,12 +4,16 @@ using MongoDB.Bson.Serialization.Attributes;
 namespace Portal.API.Models;
 
 [BsonIgnoreExtraElements]
-public class Organisation
+public class DrivingSchool
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
     
+    [BsonElement("ProductionUnitNumber")]
+    public int ProductionUnitNumber { get; set; }
+    [BsonElement("OrganisationNumber")]
+    public int OrganisationNumber { get; set; }
     [BsonElement("Name")]
     public string Name { get; set; }
     [BsonElement("Address")]
@@ -18,7 +22,5 @@ public class Organisation
     public int Zip { get; set; }
     [BsonElement("City")]
     public string City { get; set; }
-    [BsonElement("DrivingSchools")]
-    public IList<DrivingSchool> DrivingSchools { get; set; }
-
+    
 }
