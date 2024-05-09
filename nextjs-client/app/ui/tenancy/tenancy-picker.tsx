0bc9f046f4f8d73e33b981f7caf72e3deac0e172
Peter Melander <petermelander2910@gmail.com>
2024-05-09 18:22:48 +0200

    add tenant menu button

--- nextjs-client/app/ui/tenancy/tenancy-picker.tsx
+++ nextjs-client/app/ui/tenancy/tenancy-picker.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { PowerIcon } from '@heroicons/react/24/outline';
+import { PlusIcon, PowerIcon } from '@heroicons/react/24/outline';
 import { Api } from '@/app/lib/api/backoffice-api';
 import { Api as AUTHAPI } from '@/app/lib/api/auth-api';
 import { useQuery } from '@tanstack/react-query';
@@ -65,6 +65,10 @@ export default function TenancyPicker({
             </Link>
           );
         })}
+        <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            <PlusIcon className="w-6" />
+            <div className="hidden md:block">New organisation</div>
+          </button>
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form>
           <button onClick={()=> handleLogout()} className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
